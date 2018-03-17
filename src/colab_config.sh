04e5d84b79d549cfd9aeebf577e170630a8e58ce
Igor <zharikovigor97@gmail.com>
2018-03-17 18:37:03 +0300

    Add models folder to colab

--- src/colab_config.sh
+++ src/colab_config.sh
@@ -1,7 +1,8 @@
 #!/bin/bash 
-pip install keras
-pip install kaggle
+pip install keras > /dev/null
+pip install kaggle > /dev/null
 apt-get install tree
+mkdir ../models
 mkdir ../data && cd ../data
 kaggle competitions download -c state-farm-distracted-driver-detection -p .
 mv state-farm-distracted-driver-detection/* . && rm -rf state-farm-distracted-driver-detection

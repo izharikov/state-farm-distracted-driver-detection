c5c9899ee6dca3697e227fb7df0fe7676b76ae85
Igor <zharikovigor97@gmail.com>
2018-03-18 22:51:57 +0300

    modify predict to apply various model types

--- src/predict.sh
+++ src/predict.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 filename=submission-$(date -u +"%m-%d-%Y-%H-%M").csv
-model=$1
-python main.py --mode predict --path_to_model /content/drive/models/$model --output_file /content/drive/submissions/$filename
+model=$2 # path_to_model param
+python main.py --mode predict --path_to_model /content/drive/models/$model --output_file /content/drive/submissions/$filename $*
 # kaggle competitions submit -c state-farm-distracted-driver-detection -f /content/drive/submissions/$filename -m "Message"

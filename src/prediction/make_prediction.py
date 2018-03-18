cdb2931021063050862e0e1661c5ff2ee4fb7096
Igor <zharikovigor97@gmail.com>
2018-03-18 15:28:40 +0300

    fix outfile

--- src/prediction/make_prediction.py
+++ src/prediction/make_prediction.py
@@ -19,6 +19,6 @@ def make_prediction(path_to_model, output_file_csv, steps=None):
     f = open(output_file_csv, 'wt')
     f.write('img,c0,c1,c2,c3,c4,c5,c6,c7,c8,c9\n')
     for i in range(0,len(filenames)):
-        f.write("{0}, {1}\n".format(filenames[i], np.array2string(result[i],max_line_width=1000,separator=',')[1:-1]))
+        f.write("{0}, {1}\n".format(filenames[i], np.array2string(result[i],max_line_width=1000,separator=',',suppress_small=True)[1:-1]))
     f.close()
     return result

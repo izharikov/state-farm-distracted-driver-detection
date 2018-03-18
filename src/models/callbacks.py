6fbe7cc76b11745cdb486378d3266bf838c8df31
Igor <zharikovigor97@gmail.com>
2018-03-18 16:32:13 +0300

    colab doesn't work well with keras-tqdm

--- src/models/callbacks.py
+++ src/models/callbacks.py
@@ -4,9 +4,8 @@ from keras.callbacks import ModelCheckpoint
 
 from config import models_dir
 
-from keras_tqdm import TQDMCallback
 
 def get_callbacks(base_name):
     msave = ModelCheckpoint(os.path.join(models_dir, "%s-{epoch:02d}-{val_acc:.2f}.hdf5" % base_name),
                             monitor='val_acc', verbose=1, save_best_only=True, mode='max')
-    return [msave, TQDMCallback()]
+    return [msave]

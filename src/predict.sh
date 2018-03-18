b93604ae92982fa9b2b3aa21b81bc10cc51f817c
Igor <zharikovigor97@gmail.com>
2018-03-18 16:25:16 +0300

    commerce line, as kaggle submittion api doesn't work

--- src/predict.sh
+++ src/predict.sh
@@ -2,4 +2,4 @@
 filename=submission-$(date -u +"%m-%d-%Y-%H-%M").csv
 model=$1
 python main.py --mode predict --path_to_model /content/drive/models/$model --output_file /content/drive/submissions/$filename
-kaggle competitions submit -c state-farm-distracted-driver-detection -f /content/drive/submissions/$filename -m "Message"
+# kaggle competitions submit -c state-farm-distracted-driver-detection -f /content/drive/submissions/$filename -m "Message"

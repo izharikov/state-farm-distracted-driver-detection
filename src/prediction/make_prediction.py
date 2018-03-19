312fb90cb27ae1162cd436d6e8497a239cf508f9
Igor <zharikovigor97@gmail.com>
2018-03-19 19:02:15 +0300

    fix prediction image width

--- src/prediction/make_prediction.py
+++ src/prediction/make_prediction.py
@@ -4,14 +4,14 @@ import numpy as np
 from models import get_model
 
 
-def load_model(path_to_model, model_type):
-    model = get_model(model_type, False)
+def load_model(path_to_model, model_type, img_width):
+    model = get_model(modelType=model_type, print_summary=False, img_width=img_width)
     model.load_weights(path_to_model)
     return model
 
 
-def make_prediction(path_to_model, output_file_csv, model_type, steps=None, img_width = 150):
-    model = load_model(path_to_model, model_type)
+def make_prediction(path_to_model, output_file_csv, model_type, steps=None, img_width=150):
+    model = load_model(path_to_model, model_type, img_width=img_width)
     generator = get_test_datagen(img_width)
     result = model.predict_generator(generator=generator, verbose=1, workers=8, use_multiprocessing=True,
                                      steps=steps)
@@ -20,7 +20,7 @@ def make_prediction(path_to_model, output_file_csv, model_type, steps=None, img_
         filenames = filenames[0:steps * generator.batch_size]
     f = open(output_file_csv, 'wt')
     f.write('img,c0,c1,c2,c3,c4,c5,c6,c7,c8,c9\n')
-    for i in range(0,len(filenames)):
-        f.write("{0}, {1}\n".format(filenames[i], np.array2string(result[i],max_line_width=1000,separator=',')[1:-1]))
+    for i in range(0, len(filenames)):
+        f.write("{0}, {1}\n".format(filenames[i], np.array2string(result[i], max_line_width=1000, separator=',')[1:-1]))
     f.close()
     return result

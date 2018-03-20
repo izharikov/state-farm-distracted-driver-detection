22ba93a93099e73fcb173fca68d68a0b141ed279
Igor <zharikovigor97@gmail.com>
2018-03-21 00:09:54 +0300

    changed model checkpoint format

--- src/models/callbacks.py
+++ src/models/callbacks.py
@@ -6,6 +6,6 @@ from config import models_dir
 
 
 def get_callbacks(base_name):
-    msave = ModelCheckpoint(os.path.join(models_dir, "%s-{epoch:02d}-{val_acc:.2f}.hdf5" % base_name),
-                            monitor='val_acc', verbose=1, save_best_only=True, mode='max')
+    msave = ModelCheckpoint(os.path.join(models_dir, "%s-{epoch:02d}-acc-{val_acc:.4f}-loss-{val_loss:.4f}.hdf5" % base_name),
+                            monitor='val_acc', verbose=1, save_best_only=True, mode='min')
     return [msave]

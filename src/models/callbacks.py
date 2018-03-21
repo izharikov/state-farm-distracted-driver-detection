2a23ad8b9ce903790dd2cc7bb862638093a57f39
izharikov <zharikovigor97@gmail.com>
2018-03-21 09:23:25 +0300

    Update callbacks.py

--- src/models/callbacks.py
+++ src/models/callbacks.py
@@ -7,5 +7,5 @@ from config import models_dir
 
 def get_callbacks(base_name):
     msave = ModelCheckpoint(os.path.join(models_dir, "%s-{epoch:02d}-acc-{val_acc:.4f}-loss-{val_loss:.4f}.hdf5" % base_name),
-                            monitor='val_acc', verbose=1, save_best_only=True, mode='min')
+                            monitor='val_acc', verbose=1, mode='min')
     return [msave]

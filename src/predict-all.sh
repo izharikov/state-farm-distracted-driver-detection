cb67d8071f2ecdc2ee5771fbfe3caa96214ef31a
izharikov <zharikovigor97@gmail.com>
2018-03-30 12:29:50 +0300

    Update predict-all.sh

--- src/predict-all.sh
+++ src/predict-all.sh
@@ -3,7 +3,7 @@
 echo $*
 for model in /content/drive/models/*
 do 
-  filename=submission-from-$model-$(date -u +"%d-%m").csv
+  filename=submission-from-$(basename $model)-$(date -u +"%d-%m").csv
   python3 main.py --mode predict --path_to_model $model --output_file /content/drive/submissions/$filename $*
 done
 # kaggle competitions submit -c state-farm-distracted-driver-detection -f /content/drive/submissions/$filename -m "Message"

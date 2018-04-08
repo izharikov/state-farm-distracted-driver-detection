7cb2821d206700098a8319a8d2fe60b64a0e8c68
Igor <zharikovigor97@gmail.com>
2018-04-08 15:14:34 +0300

    add kernel regularizers

--- src/models/fc_layers.py
+++ src/models/fc_layers.py
@@ -1,11 +1,14 @@
 from keras.layers import Dense, Dropout
+from keras import regularizers
 
 
 def add_fc_layers(x, fc_layers, fc_dropout_layers):
     for i in range(0, len(fc_layers)):
         units = fc_layers[i]
         dropout = fc_dropout_layers[i]
-        x = Dense(units, activation='relu')(x)
+        x = Dense(units, activation='relu',
+                  kernel_regularizer=regularizers.l2(0.01),
+                  activity_regularizer=regularizers.l1(0.01))(x)
         if (dropout > 0):
             x = Dropout(dropout)(x)
     return x
